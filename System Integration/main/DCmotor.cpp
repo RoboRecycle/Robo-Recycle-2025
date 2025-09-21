60eb23a48db280bbde368311d7be62b0ac7b5ed7
Kwentn <z5479190@ad.unsw.edu.au>
2025-09-21 15:37:37 +1000

    DCmotor Implementation
    Implemented:
    - DCmotor_Init()
    - Motor_Forward()
    - Motor_Backward()
    - Motor_Brake()

--- System Integration/main/DCmotor.cpp
+++ System Integration/main/DCmotor.cpp
@@ -1,17 +1,43 @@
 #include "DCmotor.h"
 
+/*
+  Motor Control Truth Table:
+
+  IN1  IN2  ENA/ENB   Motor Behavior
+  -------------------------------
+   0    0     x      Brake (motor stops)
+   1    1     x      Invalid / Vacant
+   1    0     1      Forward at full speed
+   0    1     1      Reverse at full speed
+   1    0    PWM     Forward at PWM speed
+   0    1    PWM     Reverse at PWM speed
+
+  Notes:
+  - IN1/IN2 control the direction.
+  - ENA/ENB control the speed via PWM (0–255).
+  - 'x' means don't care (can be 0 or 1).
+  - The (1,1) state is typically avoided to prevent conflict in the H-bridge.
+*/
+
 void DCmotor_Init() {
-  // TODO: implement initialization
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(ENA, OUTPUT);
 }
 
 void Motor_Forward(int speed) {
-  // TODO: implement forward control
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, speed);
 }
 
 void Motor_Backward(int speed) {
-  // TODO: implement backward control
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, speed);
 }
 
 void Motor_Brake() {
-  // TODO: implement brake
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
 }

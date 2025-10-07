90e962cc9550ba9735ca5f3c3bfdcbfab692e874
JamieStodart <z5479061@ad.unsw.edu.au>
2025-10-07 19:48:51 +1100

    Implemented stepper cpp functions
    Used the existing code that runs the gantry - found in Arduino files/HomeMoveWithPy
    /HomeMoveWithPy.ino
    to implement the stepper cpp.

--- System Integration/main/STEPPERmotor.cpp
+++ System Integration/main/STEPPERmotor.cpp
@@ -1,25 +1,71 @@
 #include "STEPPERmotor.h"
 
 void STEPPERmotor_Init() {
-  // TODO: implement stepper initialization
+  pinMode(X_ENDSTOP_PIN, INPUT_PULLUP);
+  pinMode(Y_ENDSTOP_PIN, INPUT_PULLUP);
+  pinMode(Z_ENDSTOP_PIN, INPUT_PULLUP);
+
+  stepperX.setMaxSpeed(1000);
+  stepperX.setAcceleration(200);
+
+  stepperY.setMaxSpeed(1000);
+  stepperY.setAcceleration(200);
+
+  stepperZ.setMaxSpeed(1000);
+  stepperZ.setAcceleration(200);
 }
 
 void Stepper_HomeX() {
-  // TODO: implement X homing
+  homeAxis(stepperX, X_ENDSTOP_PIN, "X");
 }
 
 void Stepper_HomeY() {
-  // TODO: implement Y homing
+  homeAxis(stepperY, Y_ENDSTOP_PIN, "Y");
 }
 
 void Stepper_HomeZ() {
-  // TODO: implement Z homing
+  homeAxis(stepperZ, Z_ENDSTOP_PIN, "Z");
 }
 
 void Stepper_HomeAll() {
-  // TODO: implement homing all axes
+  homeAxis(stepperX, X_ENDSTOP_PIN, "X");
+  homeAxis(stepperY, Y_ENDSTOP_PIN, "Y");
+  homeAxis(stepperZ, Z_ENDSTOP_PIN, "Z");
 }
 
+// Currently should do each individually, could be made to all move at once later
+
 void Stepper_MoveTo(float x_mm, float y_mm, float z_mm) {
-  // TODO: implement coordinated move
+  long x_steps = x_mm * STEPS_PER_MM_XY;
+  long y_steps = y_mm * STEPS_PER_MM_XY;
+  long z_steps = z_mm * STEPS_PER_MM_Z;
+  stepperX.moveTo(-x_steps);
+  stepperY.moveTo(-y_steps);
+  stepperZ.moveTo(z_steps);
+}
+
+// === HOMING FUNCTION TEMPLATE ===
+void homeAxis(AccelStepper &stepper, int endstopPin, const char* axisName) {
+  const int homingSpeed = 200;
+
+  stepper.setMaxSpeed(homingSpeed);
+  stepper.setAcceleration(50);
+  if (strcmp(axisName, "Z") == 0) {
+    stepper.moveTo(-100000);  // Z direction is opposite
+  } else {
+    stepper.moveTo(100000);  // Move far in negative direction
+  }
+  
+  Serial.print("Homing ");
+  Serial.println(axisName);
+
+  while (digitalRead(endstopPin) != LOW) {
+    stepper.run();
+  }
+
+  stepper.stop();
+
+  stepper.setCurrentPosition(0);
+  Serial.print("Homed ");
+  Serial.println(axisName);
 }
--- System Integration/main/STEPPERmotor.h
+++ System Integration/main/STEPPERmotor.h
@@ -3,6 +3,29 @@
 
 #include <Arduino.h>
 
+// === CONFIG ===
+#define MOTOR_INTERFACE_TYPE AccelStepper::DRIVER
+#define STEPS_PER_MM_XY 9.09  // 200 steps / 22 mm
+#define STEPS_PER_MM_Z 20     // 500 steps / 25 mm
+
+// === X AXIS ===
+#define X_STEP_PIN 2
+#define X_DIR_PIN 5
+#define X_ENDSTOP_PIN 10
+AccelStepper stepperX(MOTOR_INTERFACE_TYPE, X_STEP_PIN, X_DIR_PIN);
+
+// === Y AXIS ===
+#define Y_STEP_PIN 3
+#define Y_DIR_PIN 6
+#define Y_ENDSTOP_PIN 9
+AccelStepper stepperY(MOTOR_INTERFACE_TYPE, Y_STEP_PIN, Y_DIR_PIN);
+
+// === Z AXIS ===
+#define Z_STEP_PIN 4
+#define Z_DIR_PIN 7
+#define Z_ENDSTOP_PIN 11
+AccelStepper stepperZ(MOTOR_INTERFACE_TYPE, Z_STEP_PIN, Z_DIR_PIN);
+
 /**
  * @brief Initialize stepper motor drivers.
  */

47a4c65d97bdba666d66a66199b33535a365ed1f
Ela Yohannan <ela.yohannan@gmail.com>
2025-11-04 10:28:38 +1100

    Update Loadcell pins & functions

--- System Integration/main/Loadcell.h
+++ System Integration/main/Loadcell.h
@@ -4,8 +4,8 @@
 #include <Arduino.h>
 
 // === CONFIG ===
-#define LOADCELL_DOUT_PIN 52   // Data pin
-#define LOADCELL_SCK_PIN 50  // Clock pin
+#define LOADCELL_DOUT_PIN A14   // Data pin
+#define LOADCELL_SCK_PIN A15  // Clock pin
 #define LOADCELL_CALIBRATION_FACTOR 2280   // To be calibrated
 #define LOADCELL_READING_NUMBER 5
 
